662fa4d93b2e6f6979c6d731635f93bc06311386
Nimish Yadav <nimishy@andrew.cmu.edu>
2024-06-12 14:07:07 -0400

    Additional arguments, Updated Documentation and format, Supports New runner version.

--- sdt_dask/examples/rev_loc_base_dask.py
+++ sdt_dask/examples/rev_loc_base_dask.py
@@ -1,16 +1,46 @@
-"""
-Local test script for Dask using local data plug
+"""Script for testing the Dask Runner tool with the LocalFiles DataPlug. The 
+script takes in arguments for number of workers, threads and amount of memory 
+allocated to each worker.
 
-It takes in the following arguments:
-    -w:int      worker number     (default=4)
-    -t:int      thread number     (default=2)
-    -m:int      memory            (default=5)
-    -v:bool     verbose           (default=False)
-    -f:string   file path         (default=None)
-    -r:string   result path       (default='../results/')
+Options:
+    -w, --workers
+            Number of Workers spawned in the Dask Client.
+            Default is 4
+    
+    -t, --threads
+            Number of threads allocated to each worker.
+            Default is 2.
 
-Example command to run this test script:
-    python rev_loc_base_dask.py -w 2 -t 2 -m 6 -f ../example_data/ -p ../results/
+    -m, --memory
+            Amount of memory in GB to allocate per worker.
+            Default is 6.
+
+    -n, --number_files
+            Number of files in directory to perform computations on. 0 indicates
+            all files in the directory path will be included.
+            Default is 0.
+            Optional
+
+    -v, --verbose
+            Enabling verbose for the Dask Runner Tool functions.
+            Default is False.
+            Optional.
+
+    -p, --perf_report
+            Enables performance report generation for the computations.
+            Default is False.
+            Optional
+
+    -f, --file
+            Directory path to the folder containing the files for computation.
+
+    -r, --result
+            Directory path to store the results and reports.
+            Default is `../results/`.
+
+Usage Examples:
+    python rev_loc_base_dask.py -w 2 -t 2 -m 6 -p -d ../example_data/ -r ../results/
+    python rev_loc_base_dask.py -w 2 -t 2 -m 6 -v -d ../example_data/
 """
 import glob, os, argparse
 from sdt_dask.dataplugs.csv_plug import LocalFiles
@@ -24,8 +54,8 @@ parser.add_argument(
     "--workers",
     default=4,
     help=(
-        "Declare number of workers. "
-        "Example --workers 3', default=4"),
+        "Number of Workers to spawn in the Dask Client. "
+        "Example: --workers 3', default=4"),
 )
 
 parser.add_argument(
@@ -33,17 +63,27 @@ parser.add_argument(
     "--threads",
     default=2,
     help=(
-        "Declare number of threads per worker. "
-        "Example --threads 3', default=2"),
+        "Number of threads allocated to each worker. "
+        "Example: --threads 3', default=2"),
 )
 
 parser.add_argument(
     "-m",
     "--memory",
-    default=5,
+    default=6,
     help=(
-        "Declare memory limit per worker. "
-        "Example --memory 5', default=5"),
+        "Amount of memory in GB to allocate per worker. "
+        "Example: --memory 5', default=5"),
+)
+
+parser.add_argument(
+    "-n",
+    "--number_files", 
+    default=0,
+    help=(
+        "Number of files in directory to perform computations on. 0 indicates \
+            all files in the directory path will be included. "
+        "Example: -n 10 --number_files 10, default=0"),
 )
 
 parser.add_argument(
@@ -52,49 +92,68 @@ parser.add_argument(
     default=False,
     action='store_true',
     help=(
-        "Enable verbose for the dask client. "
+        "Enabling verbose for the Dask Runner Tool functions. "
         "Example --verbose"),
 )
 
 parser.add_argument(
-    "-f",
-    "--file_path",
+    "-p",
+    "--perf_report",
+    default=False, 
+    action='store_true', 
+    help=(
+        "Enables performance report generation for the computations.  "
+        "Example -p"
+    )
+)
+
+parser.add_argument(
+    "-d",
+    "--dir_path",
     default=None,
     help=(
-        "Provide the directory path for file computation. "
+        "Directory path to the folder containing the files for computation. "
         "Example --file ../results/', default=None"),
 )
 
 parser.add_argument(
     "-r",
-    "--result_path",
+    "--report_dir",
     default="../results/",
     help=(
-        "Provide the result path. "
+        "Directory path to store the results and reports."
         "Example --result ../results/', default='../results/'"),
 )
 
-options = parser.parse_args()
-
-WORKERS = int(options.workers)
-THREADS = int(options.threads)
-MEMORY = float(options.memory)
-VERBOSE = bool(options.verbose)
-FILE_PATH = options.file_path
-RESULT_PATH = options.result_path
-RESULT_DIR = f"{RESULT_PATH}/{WORKERS}workers-{THREADS}threads/"
-
-# Defined Local CSV dataplug
-data_plug = LocalFiles(path_to_files=FILE_PATH)
-KEYS = [(os.path.basename(fname)[:-4],) for fname in glob.glob(FILE_PATH + "*")]
-
-os.makedirs(RESULT_DIR, exist_ok=True)
-
-# Setup the dask local client and dask tool
-# Uses the dask tool for computation
-# The conditional statement is required to allow
-# process spawning
+# Assigns Arguments to variables, configures the DataPlug and the Dask Client.
+# Calls Runner and configures the Dask task graph using Runner.set_up(). 
+# Executes the task graph on the Dask Client depending on the users preference 
+# for a performance report.
 if __name__ == '__main__':
+
+    options = parser.parse_args()
+
+    # Argument variable declaration
+    WORKERS = int(options.workers)
+    THREADS = int(options.threads)
+    MEMORY = float(options.memory)
+    NUMBER_FILES = int(options.number_files)
+    VERBOSE = bool(options.verbose)
+    PERF_REPORT = bool(options.perf_report)
+    FILE_PATH = options.dir_path
+    REPORT_DIR = options.report_dir
+    RESULT_DIR = f"{REPORT_DIR}/{WORKERS}-workers_{THREADS}-threads/"
+
+    # Defined LocalFiles DataPlug to read local CSV files
+    data_plug = LocalFiles(path_to_files=FILE_PATH)
+    KEYS = [(os.path.basename(fname)[:-4],) for fname in glob.glob(FILE_PATH + "*")]
+    
+    if NUMBER_FILES > 0: 
+        KEYS = KEYS[:NUMBER_FILES]
+
+    # checks and creates result directory if it doesn't exist
+    os.makedirs(RESULT_DIR, exist_ok=True)
+
     # Dask Local client Setup
     client_setup = LocalClient(workers=WORKERS,
                                threads=THREADS,
@@ -103,10 +162,15 @@ if __name__ == '__main__':
     client = client_setup.init_client()
 
     # Dask Tool initialization and set up
-    dask_tool = Runner(data_plug=data_plug,
-                       client=client,
+    dask_tool = Runner(client=client,
                        output_path=RESULT_DIR)
-    dask_tool.set_up(KEYS, fix_shifts=True, verbose=VERBOSE)
+    dask_tool.set_up(KEYS, data_plug, fix_shifts=True, verbose=VERBOSE)
 
-    # Dask Tool Task Compute and client shutdown
-    dask_tool.get_result()
+    if PERF_REPORT:
+        # Dask Tool Task Compute and client shutdown
+        df = dask_tool.compute_report()
+    else:
+        df = dask_tool.compute()
+
+    print(df)
+    client.shutdown()
